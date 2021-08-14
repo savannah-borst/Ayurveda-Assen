406d35a660d493d150c07578fff0e983c50f9040
Savannah Borst <77386698+savannah-borst@users.noreply.github.com>
2021-08-14 13:55:00 +0200

    aanpassingen + tekst

--- resources/script/script.js
+++ resources/script/script.js
@@ -1,7 +1,26 @@
-function openNav() {
-    document.getElementById("mySideNav").style.width = '50%';
+function openNavFunction() {
+    sideNav.style.width = '50%';
 }
 
-function closeNav() {
-    document.getElementById("mySideNav").style.width = "0%";
+function closeNavFunction() {
+    sideNav.style.width = "0%";
 }
+
+const navClose = document.getElementById("closenav");
+const navOpen = document.getElementById("opennav");
+const sideNav = document.getElementById("mySideNav");
+
+if (navClose) {
+    navClose.addEventListener('click', event => {
+        closeNavFunction();
+    })
+}
+
+if (navOpen) {
+    navOpen.addEventListener('click', event => {
+        openNavFunction();
+    })
+}
+
+
+
